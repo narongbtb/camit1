9b771d64ee451855793d155ea74e4808ba1e8b46
narongbtb <narongsao2025@gmail.com>
2026-06-22 11:16:57 +0700

    first commit

---
+++ src/main/java/com/zib/camit1/Camit1Application.java
@@ -0,0 +1,13 @@
+package com.zib.camit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Camit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Camit1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zib/camit1/Camit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zib.camit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Camit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
